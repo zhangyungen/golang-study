7142e6935e741b65aa12823087c631d1d3d21936
genyun <zyj1@truesightai.com>
2025-10-23 16:41:39 +0800

    delete: 移除拉卡拉代码

--- lakala/auth/verifier.go
+++
@@ -1,9 +0,0 @@
-package auth
-
-import "crypto/x509"
-
-// Verifier 定义验签行为
-type Verifier interface {
-	Verify(serial string, message []byte, signature string) (bool, error)
-	ValidCertificate() (*x509.Certificate, error)
-}
